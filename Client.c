aa25bae4da641ac55b4e71b657c74b2933ac672e
JaretLynch <90110874+JaretLynch@users.noreply.github.com>
2023-11-09 15:49:14 -0500

    Update Client.c

--- Client.c
+++ Client.c
@@ -1018,7 +1018,9 @@ int ClientExists(char* IP){
 
 						strncpy(Mess, DataReceived, 7);
 
-/*						printf("RECEIVED form server: *%s*\n",DataReceived);*/
+						
+
+						printf("RECEIVED form server: *%s*\n",DataReceived);
 
 /*						if (strcmp(Mess,"REFRESH")==0){	*/
 

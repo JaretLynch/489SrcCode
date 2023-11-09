5ab38528b640926bdf676235b6a75babc66f715a
JaretLynch <90110874+JaretLynch@users.noreply.github.com>
2023-11-09 17:38:41 -0500

    Update Server.c

--- Server.c
+++ Server.c
@@ -868,15 +868,15 @@ void SendMessage(char *Command,char *Arg1,char *Arg2,char *SenderIP,char *DataRe
 
 	Parse1(&Command,&Arg1,&Arg2,DataReceived);
 
-/*	Arg1[strlen(Arg1)-1]='\0';*/
+	Arg1[strlen(Arg1)-1]='\0';
 
-/*	printf("COMMAND IS *%s*\n",Command);*/
+	printf("COMMAND IS *%s*\n",Command);
 
-/*		printf("Arg1 IS *%s*\n",Arg1);*/
+		printf("Arg1 IS *%s*\n",Arg1);
 
-/*			printf("Arg2 IS *%s*\n",Arg2);*/
+			printf("Arg2 IS *%s*\n",Arg2);
 
-/*				printf("SenderIP IS *%s*\n",SenderIP);*/
+				printf("SenderIP IS *%s*\n",SenderIP);
 
 	if (strcmp(Command,"SEND")==0){
 
@@ -1847,6 +1847,8 @@ char* statistics(const Client LIST[]) {
 									strcpy(SenderIP,GetIPAddress(sock_index));
 
 									if (strcmp(Command,"SEND")==0){
+
+										printf("SEND TRIGGERED\n");
 
 										SendMessage(Command,Arg1,Arg2,SenderIP,DataReceived,sock_index);
 
